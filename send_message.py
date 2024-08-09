e358520d7058e2fe6be1f0f3025ce92ced5e6543
DING Yuanying <ding.yuanying@my.cityu.edu.hk>
2024-08-09 13:45:05 +0800

    Create send_message.py

---
+++ send_message.py
@@ -0,0 +1,20 @@
+import requests
+
+def send_message():
+    url = 'https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=cb6c67a2-8e6e-46cd-95fb-12014d909d5b'
+    headers = {'Content-Type': 'application/json'}
+    data = {
+        "msgtype": "text",
+        "text": {
+            "content": "abc很好"
+        }
+    }
+
+    response = requests.post(url, headers=headers, json=data)
+    if response.status_code == 200:
+        print("Message sent successfully!")
+    else:
+        print(f"Failed to send message. Status code: {response.status_code}, Response: {response.text}")
+
+if __name__ == "__main__":
+    send_message()
